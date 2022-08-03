9c2315d63302b2ab488b594d0c988051dd77ab35
trungjason <trungtkt159@gmail.com>
2022-08-03 21:22:30 +0700

    Complete 90%

--- freelancer/LukeNguyen_Portfolio_Shellsite/scripts/work.js
+++ freelancer/LukeNguyen_Portfolio_Shellsite/scripts/work.js
@@ -5,19 +5,44 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const workDetailWrapper = document.querySelector(".work-detail-wrapper");
     let currentBtnActive = btnList[0];
 
+    // const projects = [
+    //     { 
+    //         dataProjectType: "",
+    //         dataAos: "zoom-in-left", 
+    //         dataAosDuration: "2000",
+    //         isWhiteColor: false,
+    //         h4WorkHeading: "",
+    //         h1WorkHeading: "",
+    //     }
+    // ];
+
     btnList.forEach( (btn) => {
         btn.addEventListener("click", (e) => {
+            // If clicked button not contain class active that mean it not the current 
             if ( !e.target.classList.contains("active") ){
                 currentBtnActive.classList.remove("active");
                 currentBtnActive = e.target;
                 e.target.classList.add("active");
-            }
+                
+                const btnDataType = currentBtnActive.getAttribute('data-project-type');
 
-            if ( e.target.innerHTML === "SIDE PROJECT" ){
-                workDetailWrapper.classList.add('d-none');
-            } else {
-                workDetailWrapper.classList.remove('d-none');
-            }
+                const workDetails = workDetailWrapper.querySelectorAll('.work-detail');
+
+                workDetails.forEach( workDetail => {
+                    const workDetailType = workDetail.getAttribute('data-project-type');
+
+                    if ( btnDataType === "ALL") {
+                        workDetail.classList.remove('d-none');
+                        return;
+                    };
+
+                    if ( btnDataType === workDetailType) {
+                        workDetail.classList.remove('d-none');
+                    } else {
+                        workDetail.classList.add('d-none');
+                    };
+                });
+            };    
         })
     });
 });
